560df85e9df5ffe952340f9dd157e57b5093053c
Joshua Rizek <Joshua Rizek>
2020-08-29 12:13:51 -0400

    added morgan and made post request functional

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import express from "express"
+import morgan from "morgan"
 import characters from "./data/characters.json"
 import _ from "lodash"
 import characterRoutes from "./routes/characterRoutes"
+import bodyParser from "body-parser"
 
 const PORT = 3000
 
 const server = express();
+
+server.use(morgan('tiny'))
+server.use(bodyParser.json())
 
 const buildURL = (version , path) => `/api/${version}/${path}`;
 const CHARACTER_BASE_URL = buildURL('v1', 'characters')
--- routes/characterRoutes.js
+++ routes/characterRoutes.js
@@ -4,17 +4,19 @@ import _ from "lodash"
 
 const router = express.Router()
 
+let charactersArr = characters
+
 router.get('/', (req, res) => {
-    res.json(characters)
+    res.json(charactersArr)
 })
 
-router.get(`/:id`, (req , res) => {
-const character = _.find(characters, character => character.id === req.params.id);
-if (character) {
-    res.json(character)
-} else {
-    res.send(`User ${req.params.id} does not exist`)
-}
+router.get(`/:id`, (req, res) => {
+    const character = _.find(charactersArr, character => character.id === req.params.id);
+    if (character) {
+        res.json(character)
+    } else {
+        res.send(`User ${req.params.id} does not exist`)
+    }
 })
 
 //  trying to make a route that returns lists of characters based on occupation
@@ -28,30 +30,39 @@ if (character) {
 //     }
 //     })
 
-router.post('/', (req,res) => {
-console.log('handling post request...')
-res.end()
+router.post('/', (req, res) => {
+    console.log('handling post request...')
+    console.log(req.body)
+    //could do validations
+    charactersArr.push(req.body)
+    res.status(200).send("Alright!")
 })
 
-router.put('/', (req,res) => {
-console.log('handling put request...')
-res.end()
+router.put('/', (req, res) => {
+    console.log('handling put request...')
+    res.end()
 })
 
-router.delete('/', (req,res) => {
-console.log('handling delete request...')
-res.end()
+router.delete('/', (req, res) => {
+    console.log('handling delete request...')
+    res.end()
 })
 
-router.get('/route-handlers', (req, res, next)=>{
-res.send('learning route-handlers is fun')
-next()
-},(req, res, next)=>{
-console.log('second handler!')
-next()
-},(req,res)=>{
-console.log('third handler!!')
-}
-)
+router.get('/route-handlers', (req, res, next) => {
+    res.send('learning route-handlers is fun')
+    next()
+}, (req, res, next) => {
+    console.log('second handler!')
+    next()
+}, (req, res) => {
+    console.log('third handler!!')
+})
+
+router.param('id',(req, res, next, id) => {
+    if(isNaN(id)){
+        next(`${id} is not a valid number`);
+    }
+
+})
 
 module.exports = router;
