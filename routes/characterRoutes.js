fc88d709bb9c820eeccd14cecc24a4d3d3370a8c
Joshua Rizek <Joshua Rizek>
2020-09-08 11:24:05 -0400

    full CRUD functionality working

--- routes/characterRoutes.js
+++ routes/characterRoutes.js
@@ -36,16 +36,30 @@ const CharacterModel = mongoose.model('Character', CharacterSchema)
 
 
 router.get('/', (req, res) => {
-    res.json(charactersArr)
+    // res.json(charactersArr)
+    CharacterModel.find((err, characters) => {
+        if (err) res.status(500).send(err)
+        res.json(characters)
+    })
 })
 
 router.get(`/:id`, (req, res) => {
-    const character = _.find(charactersArr, character => character.id === req.params.id);
-    if (character) {
-        res.json(character)
-    } else {
-        res.send(`User ${req.params.id} does not exist`)
-    }
+    CharacterModel.findById(req.params.id,(err,character)=>{
+        if (err) res.status(500).send(err)
+        if (character) {
+                res.json(character)
+            } else {
+                res.status(404).send(`User ${req.params.id} does not exist`)
+            }
+    })
+
+
+    // const character = _.find(charactersArr, character => character.id === req.params.id);
+    // if (character) {
+    //     res.json(character)
+    // } else {
+    //     res.send(`User ${req.params.id} does not exist`)
+    // }
 })
 
 //  trying to make a route that returns lists of characters based on occupation
@@ -69,14 +83,31 @@ router.post('/', (req, res) => {
     })
 })
 
-router.put('/', (req, res) => {
-    console.log('handling put request...')
-    res.end()
+router.put('/:id', (req, res) => {
+   CharacterModel.findById(req.params.id, (err,character) => {
+   if(err) res.status(500).send(err)
+   if (character) {
+   character.name = req.body.name
+   character.image = req.body.image
+   character.alias = req.body.alias
+   character.quirk = req.body.quirk
+   character.status = req.body.status
+   character.occupation = req.body.occupation
+   character.save().then((err, character) => {
+    if(err) res.status(500).send(err)
+    res.json(character)
+   })
+    }else{
+        res.status(404).send(`User ${req.params.id} does not exist`)
+    }
+   })
 })
 
-router.delete('/', (req, res) => {
-    console.log('handling delete request...')
-    res.end()
+router.delete('/:id', (req, res) => {
+    CharacterModel.findByIdAndRemove(req.params.id, (err, character) => {
+        if (err) res.status(500).send(err)
+        res.status(200).send(`Character with id ${req.params.id} was deleted`)
+    })
 })
 
 router.get('/route-handlers', (req, res, next) => {
@@ -89,11 +120,11 @@ router.get('/route-handlers', (req, res, next) => {
     console.log('third handler!!')
 })
 
-router.param('id',(req, res, next, id) => {
-    if(isNaN(id)){
-        next(`${id} is not a valid number`);
-    }
+// router.param('id',(req, res, next, id) => {
+//     if(isNaN(id)){
+//         next(`${id} is not a valid number`);
+//     }
 
-})
+// })
 
 module.exports = router;
