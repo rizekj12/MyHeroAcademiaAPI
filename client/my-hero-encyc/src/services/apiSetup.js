0423de982398430e1d4fee6182d444979592c525
Joshua Rizek <Joshua Rizek>
2020-09-21 13:07:14 -0400

    added heroku api

--- client/my-hero-encyc/src/services/apiSetup.js
+++ client/my-hero-encyc/src/services/apiSetup.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 let apiUrl
 
 const apiUrls = {
-    production: "",
+    production: "https://mha-api.herokuapp.com/api/characters",
     development: "http://localhost:3000/api/characters/"
 }
 
